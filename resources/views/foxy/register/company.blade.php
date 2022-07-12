643e4a4384fe158e04ba5b0c32049530d8fb51ba
Stif <stiven.maldonado@symplifica.com>
2022-07-11 19:14:26 -0500

    init foxy

--- resources/views/foxy/register/company.blade.php
+++ resources/views/foxy/register/company.blade.php
@@ -1,14 +1,23 @@
 <x-foxy-layout>
     <div class="container">
+
         <div class="card mt-5">
-            <div class="card-header">
-                <h4 class="card-title">Single Layout</h4>
+            <div class="row">
+                <div class="col-sm">
+                    One of three columns
+                </div>
+                <div class="col-sm-10">
+                    <div class="card-header">
+                        <h4 class="card-title">Bienvenido a Lukran Heaven </h4>
+                    </div>
+                    <div class="card-body">
+                        <p>Hola, Mi nombre es <strong>FOXY</strong> y te guiaré por el proceso de registro de tu emprendimiento,
+                            para poderte ayudar, necesito que me cuentes algunas cosas sobre tu compañía</p>
+                    </div>
+                </div>
+
             </div>
-            <div class="card-body">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo quasi labore expedita? Veritatis
-                    at maxime id voluptates excepturi molestiae possimus blanditiis dicta consequuntur maiores suscipit,
-                    eveniet neque obcaecati doloribus.</p>
-            </div>
+
         </div>
     </div>
 </x-foxy-layout>

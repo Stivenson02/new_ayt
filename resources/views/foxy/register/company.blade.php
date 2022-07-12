4128cd50b00ab8b331b6af5804bec7354a38724a
Stif <stiven.maldonado@symplifica.com>
2022-07-11 20:12:40 -0500

    foxy img

--- resources/views/foxy/register/company.blade.php
+++ resources/views/foxy/register/company.blade.php
@@ -4,15 +4,17 @@
         <div class="card mt-5">
             <div class="row">
                 <div class="col-sm">
-                    One of three columns
+                    <div class="container d-block mt-2">
+                        <img class="img-fluid img-thumbnail-bg thumb-post" src="assets/images/foxy/foxy1-1.png">
+                    </div>
                 </div>
                 <div class="col-sm-10">
                     <div class="card-header">
                         <h4 class="card-title">Bienvenido a Lukran Heaven </h4>
                     </div>
                     <div class="card-body">
-                        <p>Hola, Mi nombre es <strong>FOXY</strong> y te guiaré por el proceso de registro de tu emprendimiento,
-                            para poderte ayudar, necesito que me cuentes algunas cosas sobre tu compañía</p>
+                        <p>Hola, Mi nombre es <strong>FOXY</strong> y te guiaré por el proceso de registro de tu emprendimiento, es un paso importante ya que así te felicitare todas las herramientas que necesites, y te podré conocer un poco más.
+                            Para poderte ayudar, necesito que me cuentes más sobre tu compañía</p>
                     </div>
                 </div>
 

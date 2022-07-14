94f97b87737b89cadbe3582f8285c6f789eda87b
Stif <stiven.maldonado@symplifica.com>
2022-07-13 23:05:57 -0500

    request change

--- app/Http/Controllers/Foxy/Stakeholder/StakeholderController.php
+++ app/Http/Controllers/Foxy/Stakeholder/StakeholderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Foxy\Stakeholder;
 
 use App\Http\Controllers\Controller;
 use App\Models\Company\Company;
-use http\Env\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class StakeholderController extends Controller
